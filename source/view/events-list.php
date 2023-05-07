091317a532d37d74be59589c943a4aee75f1e055
Daniel Waxweiler <daniel.waxweiler@pm.me>
2023-05-07 20:07:07 +0200

    wip

--- source/view/events-list.php
+++ source/view/events-list.php
@@ -4,21 +4,20 @@ if (!defined('ABSPATH')) {
   exit;
 }
 ?>
-<div class="<?php echo esc_attr($classNamePrefix); ?>_events-list">
-  <li style="display: none;"><?php esc_html_e('The events could not be loaded!', 'connector-mobilizon'); ?></li>
-  <li style="display: none;"><?php esc_html_e('The group could not be found!', 'connector-mobilizon'); ?></li>
-</ul>
-
-<ul>
-<?php foreach($data['data']['events']['elements'] as $event) { ?>
-  <li>
-    <a href="<?php echo esc_attr($event['url']); ?>"><?php echo esc_html_e($event['title']); ?></a>
-    <br>
-    <?php echo esc_html_e(Formatter::format_date($locale, $timeZone, $event['beginsOn'], $event['endsOn'], $isShortOffsetNameShown)); ?>
-    <?php if (isset($event['physicalAddress'])) { ?>
-    <br>
-    <?php echo esc_html_e(Formatter::format_location($event['physicalAddress']['description'], $event['physicalAddress']['locality'])) ?>
+<div class="<?php echo esc_attr($classNamePrefix); ?>_events-list-php">
+  <div class="general-error" style="display: none;"><?php esc_html_e('The events could not be loaded!', 'connector-mobilizon'); ?></div>
+  <div class="group-not-found" style="display: none;"><?php esc_html_e('The group could not be found!', 'connector-mobilizon'); ?></div>
+  <ul>
+    <?php foreach($data['data']['events']['elements'] as $event) { ?>
+    <li>
+      <a href="<?php echo esc_attr($event['url']); ?>"><?php echo esc_html_e($event['title']); ?></a>
+      <br>
+      <?php echo esc_html_e(Formatter::format_date($locale, $timeZone, $event['beginsOn'], $event['endsOn'], $isShortOffsetNameShown)); ?>
+      <?php if (isset($event['physicalAddress'])) { ?>
+      <br>
+      <?php echo esc_html_e(Formatter::format_location($event['physicalAddress']['description'], $event['physicalAddress']['locality'])) ?>
+      <?php } ?>
+    </li>
     <?php } ?>
-  </li>
-<?php } ?>
-</ul>
+  </ul>
+</div>
